4ba0d143561d2c4c48fef7f1fb8580fea04d2fbf
Mohammad-Taleb <akamohdtaleb@gmail.com>
2025-02-13 22:31:39 +0200

    add if-else conditions <>

--- user-authenticator.py
+++ user-authenticator.py
@@ -1,2 +1,7 @@
 user_name = input("Enter your username: ").lower()
-user_password = input("Enter your password: ")
+user_password = int(input("Enter your password: "))
+
+if user_name == 'admin' and user_password == 1234:
+  print("Access granted")
+else:
+  print("Access denied")
