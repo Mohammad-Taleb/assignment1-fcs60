44f105caa1e2f65a690e3cb06bec268d38ebe5f7
Mohammad-Taleb <akamohdtaleb@gmail.com>
2025-02-13 22:04:43 +0200

    add if-else conditions <>

--- movie-ticket-problem.py
+++ movie-ticket-problem.py
@@ -1,5 +1,15 @@
 # mohmmad taleb fcs 60
+# my first time trying git here so please excuse any mistakes, thanks 
 
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 
+if age > 0:
+    if 1 <= age <= 7:
+      print(f"{name} your price ticket is $5")
+    elif age >= 18:
+      print(f"{name} your price ticket is $15")
+    elif age <= 18:
+      print(f"{name} your price ticket is $10")
+else:
+  print("Enter a valid age")
